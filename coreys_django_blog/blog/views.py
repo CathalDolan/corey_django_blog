a3f9f152d22aac818cfc7672c317610647b10bcf
CathalDolan <cathal@gizagig.com>
2021-01-17 21:27:02 +0000

    HTML Templates created and paths added to installed apps and configuration complete. Up to 18mins on video 3

--- coreys_django_blog/blog/views.py
+++ coreys_django_blog/blog/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'author': 'Cathal Dolan',
+        'title': 'Post 1',
+        'content': 'Post 1\'s content',
+        'date_posted': 'January 17th, 2021'
+    },
+    {
+        'author': 'Liam Dolan',
+        'title': 'Post 2',
+        'content': 'Post 2\'s content',
+        'date_posted': 'January 18th, 2021'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
